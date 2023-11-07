4e308f7e8445bef97b4ce76ae1c326a206eccd6a
MEGAN BREEN <MBREEN@GICW.ORG>
2023-11-07 14:59:54 -0800

    refactored js coding

--- js/app.js
+++ js/app.js
@@ -1,75 +1,48 @@
 'use strict';
 
 let workingProducts = [];
-const allProducts= [];
+const productNames= ['bag','boots', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'];
 const leftProductImage = document.querySelector('section img:first-child');
 const centerProductImage = document.querySelector('section img:nth-child(2)');
 const rightProductImage = document.querySelector('section img:nth-child(3)');
 let clickCtr = 0;
 const maxClicks = 25;
-const viewResults = document.querySelector('button');
-let leftProductInstance = null;
-let centerProductInstance = null;
-let rightProductInstance = null;
+const viewResults = document.getElementById('viewResults');
+let leftProduct = null;
+let centerProduct = null;
+let rightProduct = null;
 const ulElem = document.querySelector('ul');
+let leftProductInstance = leftProduct;
+let rightProductInstance = rightProduct;
+let centerProductInstance = centerProduct;
+let allProducts =[]
 
-
-function Products (name, src, views, clicks) {
+function Products (name, src) {
     this.name = name;
     this.src = src;
     this.views = 0;
     this.clicks = 0;
+    allProducts.push(this);
 }
 
-let bag = new Products ('Bag', './img/bag.jpg');
-let banana = new Products ('Banana', './img/banana.jpg');
-let bathroom = new Products ('Bathroom', './img/bathroom.jpg');
-let boots = new Products ('Boots', './img/boots.jpg');
-let breakfast = new Products ('Breakfast', './img/breakfast.jpg');
-let bubblegum= new Products ('Bubblegum', './img/bubblegum.jpg');
-let chair = new Products ('Chair', './img/chair.jpg');
-let cthulhu = new Products ('Cthulhu', './img/cthulhu.jpg');
-let dogDuck = new Products ('Dog Duck', './img/dog-duck.jpg');
-let dragon = new Products ('Dragon', './img/dragon.jpg');
-let pen = new Products ('Pen', './img/pen.jpg');
-let petSweep = new Products ('Pet Sweep', './img/pet-sweep.jpg');
-let scissors = new Products ('Scissors', './img/scissors.jpg');
-let shark = new Products ('Shark', './img/shark.jpg');
-let sweep = new Products ('Sweep', './img/sweep.png');
-let tauntaun = new Products ('Taun Taun', './img/tauntaun.jpg');
-let unicorn = new Products ('Unicorn', './img/unicorn.jpg');
-let waterCan = new Products ('Water Can', './img/water-can.jpg');
-let wineGlass = new Products ('Wine Glass', './img/wine-glass.jpg');
+// Products.allProducts =[];
+Products.workingProducts = [];
 
-
-
-allProducts.push(bag);
-allProducts.push(banana);
-allProducts.push(bathroom);
-allProducts.push(boots);
-allProducts.push(breakfast);
-allProducts.push(bubblegum);
-allProducts.push(chair);
-allProducts.push(cthulhu);
-allProducts.push(dogDuck);
-allProducts.push(dragon);
-allProducts.push(pen);
-allProducts.push(petSweep);
-allProducts.push(scissors);
-allProducts.push(shark);
-allProducts.push(sweep);
-allProducts.push(tauntaun);
-allProducts.push(unicorn);
-allProducts.push(waterCan);
-allProducts.push(wineGlass);
+function createProducts(){
+    for (let i = 0; i < productNames.length; i++){
+    const productName = productNames[i];
+    const productInstance = new Products(productName, `./img/${productName}.jpg`);
+console.log(productInstance)
+//    allProducts.push(productInstance);
+}
+}
+createProducts()
+console.log(allProducts)
 
 function renderProducts() {
     if(clickCtr == maxClicks){
-        viewResults.addEventListener('click', handleViewResultsClick);
-
-        leftProductImage.removeEventListener('click', handleLeftProductClick);
-        centerProductImage.removeEventListener('click', handleCenterProductClick);
-        rightProductImage.removeEventListener('click', handleRightProductClick);
+        endVoting();
+        return;
     }
 
     if(workingProducts.length<=1){
@@ -77,19 +50,21 @@ function renderProducts() {
         shuffleArray(workingProducts);
     }
 
-    leftProductInstance = workingProducts.pop();
-    leftProductImage.setAttribute('src', leftProductInstance.src);
-    
-    rightProductInstance = workingProducts.pop();
-    rightProductImage.setAttribute('src', rightProductInstance.src);
+    leftProduct = workingProducts.pop();
+    rightProduct = workingProducts.pop();
+    centerProduct = workingProducts.pop();
 
-    centerProductInstance = workingProducts.pop();
-    centerProductImage.setAttribute('src', centerProductInstance.src);
 
-    leftProductInstance.views += 1;
-    rightProductInstance.views += 1;
-    centerProductInstance.views += 1
+    leftProduct.views += 1;
+    rightProduct.views += 1;
+    centerProduct.views += 1;
+
+    leftProductImage.setAttribute('src', leftProduct.src);
+    rightProductImage.setAttribute('src', rightProduct.src);
+    centerProductImage.setAttribute('src', centerProduct.src);
+
 }
+
 
 function shuffleArray(array) {
     for (let i = array.length -1; i > 0; i-- ){ 
@@ -98,20 +73,21 @@ function shuffleArray(array) {
     }
 }
 function handleLeftProductClick() {
-    leftProductInstance.clicks += 1;
+    leftProduct.clicks += 1;
     clickCtr += 1;
     renderProducts();
 }
 function handleRightProductClick() {
-    rightProductInstance.clicks += 1;
+    rightProduct.clicks += 1;
     clickCtr += 1;
     renderProducts();
 }
 function handleCenterProductClick() {
-    centerProductInstance.clicks += 1;
+    centerProduct.clicks += 1;
     clickCtr += 1;
     renderProducts();
 }
+
 function handleViewResultsClick(){
     renderResults();
 }
@@ -120,8 +96,14 @@ leftProductImage.addEventListener('click', handleLeftProductClick);
 rightProductImage.addEventListener('click', handleRightProductClick);
 centerProductImage.addEventListener('click', handleCenterProductClick);
 
-renderProducts();
+function endVoting(){
+    leftProductImage.removeEventListener('click', handleLeftProductClick);
+    centerProductImage.removeEventListener('click', handleCenterProductClick);
+    rightProductImage.removeEventListener('click', handleRightProductClick);
 
+   viewResults.hidden = false;
+    viewResults.addEventListener('click', handleViewResultsClick);
+}
 function renderResults() {
     for (let i =0; i<allProducts.length; i++){
         const currentProduct = allProducts[i];
@@ -131,4 +113,6 @@ function renderResults() {
         liElem.textContent = result;
     }
 }
+
+renderProducts();
 
